54b9c0423677ff394b957750021d975e7b4c2444
charanraj-kulal <charanraj9731@gmail.com>
2024-09-11 14:18:53 +0530

    changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
-import Nav from "@/components/Nav";
-import Hero from "@/components/Hero";
-import FeaturesSectionDemo from "@/components/About";
-import Products from "@/components/Products";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Nav from "../components/Nav";
+import Hero from "../components/Hero";
+import FeaturesSectionDemo from "../components/About";
+import Products from "../components/Products";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 const Home = () => {
   return (
     <>
